e83f2684f6a6ec9988e387e5093cab745b5a525d
jag cheema <jagmitsingh2003@gmail.com>
2023-04-03 02:05:59 -0400

    updated comments

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -7,27 +7,27 @@ const commentData = [
     user_id: 1
   },
   {
-    comment_text: "TEXT is another useful data type for entering long form text data.",
+    comment_text: "this is awesome thanks !",
     post_id: 1,
     user_id: 4
   },
   {
-    comment_text: "Ah, so that's what cookies are good for (besides eating ~.0)!",
+    comment_text: "cloud computing saved me so much !",
     post_id: 4,
     user_id: 2
   },
   {
-    comment_text: "Great. Now I'm hungry for cookies.",
+    comment_text: "thanks for the input !",
     post_id: 4,
     user_id: 3
   },
   {
-    comment_text: "Did you know about salting?",
+    comment_text: "Im both scared and excited about AI",
     post_id: 5,
     user_id: 5
   },
   {
-    comment_text: "Yeah! Adding that random data is a great way to force a unique hashed output, even when your users all pick 'password' for their password >.< ",
+    comment_text: "its like everytime I call customer service im just talking to an AI",
     post_id: 5,
     user_id: 4
   },
